a590fb067059718a8f8a56c84627309a5a021535
Arvind Pal <arvindpal@Arvinds-MacBook-Air.local>
2023-08-16 22:33:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const endDate = "01 January 2024 00:00 AM";
+document.getElementById("end-date").innerHTML=endDate;
+const inputs = document.querySelectorAll("input")
+
+function clock(){
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now)/1000;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+setInterval(()=>{
+clock();
+},1000)
